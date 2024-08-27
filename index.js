c36b60949ebb7dc26297b5894397c10431de20fa
lucio-souza <desouzaluioweslley@gmail.com>
2024-08-27 15:14:16 -0300

    impedindo erro de cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import router from './src/routes/userRouter.js'
+import cors from 'cors';
 
+let corsOptions = {
+    origin: ['http://127.0.0.1:5500']
+  };
 const app=express();
 
 app.use(express.json());
